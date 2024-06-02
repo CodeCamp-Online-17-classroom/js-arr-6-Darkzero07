727249bce8e6146d1f5d4624abc89a2e96357faa
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-02 08:49:52 +0700

    js lab 92

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+const alphabets = ["a", "b", "a", "c", "a", "d"];
+// expexted result: [0, 2, 4]
+
+let arr = new Array();
+
+alphabets.forEach((item, index) => {
+  if (item === "a") {
+    arr.push(index);
+  }
+});
+
+console.log(arr);
